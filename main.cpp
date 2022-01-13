7ef3f843be936969426fcf2d8b7c0ed4d7543861
aurora <56488329+auurora@users.noreply.github.com>
2022-01-13 07:28:14 +0000

    253 chars

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@
 #include <queue>
 int main()
 {
-auto* a="--[----->+<]>-----.--[--->+<]>..---.---.+++.+++[->+++<]>++.-[->+++<]>.-[--->++<]>-.++++++++++.+[---->+<]>+++.+[->+++<]>.++++++++++++..---.";
+auto* a=",....";
 std::map<int,char>m;
 std::queue<const char*>j;
 int p{};
 do{
-int v=*a,l=(m[p+=(v==62)-(v==60)]=v==44?getchar():m[p]+(v==43)-(v==45));
+int v=*a,l=m[p+=(v==62)-(v==60)]=v==44?getchar():m[p]+(v==43)-(v==45);
 if(v==91){if(l)j.push(a);else while(*++a!=93);}
 if(v==93){if(l)a=j.back();else j.pop();}
 putchar((v==46)*l);
