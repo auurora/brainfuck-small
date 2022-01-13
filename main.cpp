a8dd48b754c882c9e6e4f54b02c31d93dcdda4a9
aurora <56488329+auurora@users.noreply.github.com>
2022-01-13 06:45:01 +0000

    258 chars

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ std::map<int,char>m;
 std::queue<const char*>j;
 int p{};
 do{
-auto&l=m[p+=*a==62?1:-(*a==60)],v=*a;l+=v==43?1:-(v==45);
+char v=*a,l=m[p+=(*a==62)-(*a==60)]+=(v==43)-(v==45);
 if(v==91){if(l)j.push(a);else while(*++a!=93);}
 if(v==93){if(l)a=j.back();else j.pop();}
-std::cout<<(v==46?l:'\0');
-if(v==44)l=std::cin.get();
-}while(*(++a));
+putchar((v==46)*l);
+if(v==44)l=getchar();
+}while(*++a);
 }
