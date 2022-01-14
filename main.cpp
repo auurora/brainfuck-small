61008a470af39363e5c708eea50ce72d9af8a0d5
aurora <56488329+auurora@users.noreply.github.com>
2022-01-14 01:25:40 +0000

    formatting

--- main.cpp
+++ main.cpp
@@ -4,12 +4,9 @@
 int main()
 {
 auto* a="--[----->+<]>-----.--[--->+<]>..---.---.+++.+++[->+++<]>++.-[->+++<]>.-[--->++<]>-.++++++++++.+[---->+<]>+++.+[->+++<]>.++++++++++++..---.";
-std::map<int,char>m;
-std::stack<const char*>j;
-int p{};
+std::map<int,char>m;std::stack<const char*>j;int p{};
 while(*a){
 int v=*a++,l=m[p]=v==44?getchar():m[p+=(v==62)-(v==60)]+(v==43)-(v==45);
-v==91?l?j.push(a):[&]{while(*++a!=93);}(),a:
-v==93?a=l?j.top():(j.pop(),a):(putchar((v==46)*l),a);
+v==91?l?j.push(a):[&]{while(*++a!=93);}(),a:v==93?a=l?j.top():(j.pop(),a):(putchar((v==46)*l),a);
 }
 }
