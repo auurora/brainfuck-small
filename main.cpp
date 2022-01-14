3ca31a988e3d934d7b7a7116beb19fe0fcad7fb2
aurora <56488329+auurora@users.noreply.github.com>
2022-01-14 01:13:58 +0000

    242 chars

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ std::stack<const char*>j;
 int p{};
 while(*a){
 int v=*a++,l=m[p]=v==44?getchar():m[p+=(v==62)-(v==60)]+(v==43)-(v==45);
-if(v==91){if(l)j.push(a);else while(*++a!=93);}
+if(v==91)l?j.push(a):[&]{while(*++a!=93);}();
 if(v==93)a=l?j.top():(j.pop(),a);
 putchar((v==46)*l);
 };
