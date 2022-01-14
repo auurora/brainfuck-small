06740521b633964aa12e5034d05225f43da352ce
aurora <56488329+auurora@users.noreply.github.com>
2022-01-14 01:04:28 +0000

    250 chars

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 #include <map>
 #include <iostream>
-#include <queue>
+#include <stack>
 int main()
 {
 auto* a="--[----->+<]>-----.--[--->+<]>..---.---.+++.+++[->+++<]>++.-[->+++<]>.-[--->++<]>-.++++++++++.+[---->+<]>+++.+[->+++<]>.++++++++++++..---.";
 std::map<int,char>m;
-std::queue<const char*>j;
+std::stack<const char*>j;
 int p{};
-do{
-int v=*a,l=m[p]=v==44?getchar():m[p+=(v==62)-(v==60)]+(v==43)-(v==45);
+while(*a){
+int v=*a++,l=m[p]=v==44?getchar():m[p+=(v==62)-(v==60)]+(v==43)-(v==45);
 if(v==91){if(l)j.push(a);else while(*++a!=93);}
-if(v==93){if(l)a=j.back();else j.pop();}
+if(v==93){if(l)a=j.top();else j.pop();}
 putchar((v==46)*l);
-}while(*++a);
+};
 }
